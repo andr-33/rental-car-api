bb41bf569b801962d0b44dcd83866e7b0135058d
andr-33 <andree.serranourraca@gmail.com>
2025-09-30 13:57:51 +0200

    fix: actualizar cors para peticiones del cliente

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const routes = require('./routes');
 
 const app = express();
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: 'http://localhost:5173/',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 };
