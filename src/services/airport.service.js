b4b475e8e412f3aef7d69c98eb756facc37764a8
andr-33 <andree.serranourraca@gmail.com>
2025-10-01 11:23:06 +0200

    ft: respuesta data carros con imagen

--- src/services/airport.service.js
+++ src/services/airport.service.js
@@ -2,7 +2,7 @@ const supabase = require('../config/supabase.config');
 const aiportService = {};
 
 aiportService.getAllAirportsFromDB = async () => {
-    const { data, error } = await supabase.from('Airports').select('*');
+    const { data, error } = await supabase.from('airports').select('*');
     if (error) throw error;
     return data;
 };
--- src/services/car.service.js
+++ src/services/car.service.js
@@ -2,7 +2,13 @@ const supabase = require('../config/supabase.config');
 const carService = {};
 
 carService.getAllCarsFromDB = async () => {
-    const { data, error } = await supabase.from('Cars').select('*');
+    const { data, error } = await supabase
+        .from('cars')
+        .select(`
+            *,
+            images:car_images(*)
+        `);
+        
     if (error) throw error;
     return data;
 };
