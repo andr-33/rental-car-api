47a101880eb35e3586b488ace61eb94a37023236
andr-33 <andree.serranourraca@gmail.com>
2025-10-20 12:02:33 +0200

    ft: instalado React Email

--- src/services/mailer.service.js
+++ src/services/mailer.service.js
@@ -1,14 +1,17 @@
 const mailer = require("../config/mailer.config");
-const { from } = require("../config/supabase.config");
-mailerService = {};
+const { render } = require("@react-email/components");
+import RentalRequest from "../utils/templates/RentalRequest";
+const mailerService = {};
 
 mailerService.sendRentalRequestEmail = async (recipientEmail, carModel) => {
   try {
+    const html = await render(<RentalRequest recipientEmail={recipientEmail} carModel={carModel} />);
+
     const mailOptions = {
       from: process.env.MAILER_USER,
       to: recipientEmail,
       subject: `Solicitu de renta - ${carModel}`,
-      text: "Hola mundo"
+      html: html,
     };
     await mailer.sendMail(mailOptions);
   } catch (error) {
